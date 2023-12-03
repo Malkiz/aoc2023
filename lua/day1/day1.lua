0902ac48a579eb3731ea5640167bab58bc05754d
Malkiz <omieyal@gmail.com>
2023-12-03 20:22:58 +0200

    lua day1 part2

--- lua/day1/day1.lua
+++ lua/day1/day1.lua
@@ -15,11 +15,46 @@ if not fileContent then
   os.exit()
 end
 
+local subStrings = {
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+}
+local strToNum = {
+  zero = 0,
+  one = 1,
+  two = 2,
+  three = 3,
+  four = 4,
+  five = 5,
+  six = 6,
+  seven = 7,
+  eight = 8,
+  nine = 9
+}
+
+local function getNextIndex(line, index)
+  local minIndex = math.huge
+  local minSubString = nil
+  for _, subString in ipairs(subStrings) do
+    local subStringIndex = line:find(subString, index)
+    if subStringIndex and subStringIndex < minIndex then
+      minIndex = subStringIndex
+      minSubString = subString
+    end
+  end
+  return minIndex, minSubString
+end
+
 local sum = 0
 for line in fileContent:gmatch("[^\r\n]+") do
   local digits = {}
-  for digit in line:gmatch("%d") do
-    table.insert(digits, digit)
+  local index = 1
+  while index <= #line do
+    local nextIndex, subString = getNextIndex(line, index)
+    local digit = strToNum[subString] or subString
+    if digit then
+      table.insert(digits, digit)
+    end
+    index = nextIndex + 1
   end
 
   local firstDigit = digits[1]
